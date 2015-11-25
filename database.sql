a4e42f93adc2819b85fe7b8911d67594cc07d0ca
Daichi0119110 <daichi0119110@gmail.com>
2015-11-25 11:49:33 +0900

    database.sqlのダミーデータ以外完成

--- database.sql
+++ database.sql
@@ -1,7 +1,6 @@
 create database connect;
 
 grant all on connect.* to dbuser@localhost identified by 'connect2016';
-
 use connect;
 
 /*usersのテーブル作成*/
@@ -11,82 +10,135 @@ create table users (
     name varchar(50),
     filename varchar(255),
     university_id int,
-    self_introduction varchar(400),
-    japanese_university varchar(100),
+    birthday date,
+    gender varchar(10), /*女or男*/
+    self_intro text,
+    study_end date,
     study_start date,
-    study_finish date,
     study_grade varchar(255),
     study_major varchar(255),
-    birthday date,
-    gender varchar(50), 
+    last_login date,
     created datetime default null,
     modified datetime default null
 );
 
-/*postsのテーブル作成*/
-create table posts (
+/*areasのテーブル作成*/
+create table areas (
     id int not null auto_increment primary key,
-    user_id int,
-    university_id int,
-    title text,
-    body text,
+    area varchar(255)
+);
+
+/*countriesのテーブル作成*/
+create table countries (
+    id int not null auto_increment primary key,
+    country varchar(255),
+    area_id int
+);
+
+/*universitiesのテーブル作成*/
+create table universities (
+    id int not null auto_increment primary key,
+    university varchar(255),
+    country_id int,
     created datetime default null,
     modified datetime default null
 );
+
+/*categoriesのテーブル作成*/
+create table categories (
+    id int not null auto_increment primary key,
+    category varchar(255),
+    question text
+);
+
+/*tagsのテーブル作成*/
+create table tags (
+    id int not null auto_increment primary key,
+    tag varchar(255)
+);
+
+
 
 /*reviewsのテーブル作成*/
 create table reviews (
     id int not null auto_increment primary key,
     user_id int,
-    university_id int,
     category_id int,
-    body text,
+    university_id int,
+    content text,
     created datetime default null,
     modified datetime default null
 );
 
-
-
-/*countriesのテーブル作成*/
-create table countries (
+/*scoresのテーブル作成*/
+create table scores (
     id int not null auto_increment primary key,
-    countryname varchar(255)
-  );
+    user_id int,
+    tag_id int,
+    university_id int,
+    score int,
+    created datetime default null,
+    modified datetime default null
+);
 
-/*データの挿入*/
-insert into countries (countryname) values
-('America'),
-('Japan'),
-('Belgium'),
-('France'),
-('Italy'),
-('Korea'),
-('China'),
-('India');
-
-
-/*universitiesのテーブル作成*/
-create table universities (
+/*favorite_revsのテーブル作成*/
+create table favorite_revs (
     id int not null auto_increment primary key,
-    country_id int ,
-    universityname varchar(255)
-   );
+    fav_flg int,
+    user_id int,
+    review_id int,
+    created datetime default null,
+    modified datetime default null
+);
 
-/*データの挿入*/
-insert into universities (country_id, universityname) values 
-(3,'Ghent'),
-(1,'UCLA'),
-(2,'Keio'),
-(2,'Titech');
-
-/*categoriesのテーブル作成*/
-create table categories (
+/*favorite_usesのテーブル作成*/
+create table favorite_uses (
     id int not null auto_increment primary key,
-    categoryname varchar(50)
-    );
+    fav_flg int,
+    user_id int,
+    target int, /*target = お気に入りした人のid*/
+    created datetime default null,
+    modified datetime default null
+);
 
-/*データの挿入*/
-insert into categories (categoryname) values 
-('city'),
-('university'),
-('food');
+/*favorite_unisのテーブル作成*/
+create table favorite_unis (
+    id int not null auto_increment primary key,
+    fav_flg int,
+    user_id int,
+    university_id int,
+    created datetime default null,
+    modified datetime default null
+);
+
+/*questionsのテーブル作成*/
+create table questions (
+    id int not null auto_increment primary key,
+    user_id int,
+    question text,
+    university_id int,
+    created datetime default null,
+    modified datetime default null
+);
+
+/*answersのテーブル作成*/
+create table answers (
+    id int not null auto_increment primary key,
+    user_id int,
+    answer text,
+    university_id int,
+    created datetime default null,
+    modified datetime default null
+);
+
+/*picturesのテーブル作成*/
+create table pictures (
+    id int not null auto_increment primary key,
+    filename varchar(255),
+    university_id int,
+    user_id int,
+    comment text,
+    created datetime default null,
+    modified datetime default null
+);
+
