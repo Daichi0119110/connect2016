2f29cf4948a483083cea611d6a83312a7bc763e4
Daichi0119110 <daichi0119110@gmail.com>
2015-11-30 12:37:45 +0900

    database.sqlの修正

--- testdata/categories.sql
+++ testdata/categories.sql
@@ -1,9 +1,9 @@
-insert into categories (id, category, question) values (1, 'a', 'a');
-insert into categories (id, category, question) values (2, 'a', 'b');
-insert into categories (id, category, question) values (3, 'a', 'c');
-insert into categories (id, category, question) values (4, 'a', 'd');
-insert into categories (id, category, question) values (5, 'a', 'e');
-insert into categories (id, category, question) values (6, 'a', 'f');
-insert into categories (id, category, question) values (7, 'a', 'g');
-insert into categories (id, category, question) values (8, 'a', 'h');
-insert into categories (id, category, question) values (9, 'a', 'i');
+insert into categories (id, category, question, example, image) values (1, 'a', 'a', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'adipiscing molestie.tiff');
+insert into categories (id, category, question, example, image) values (2, 'a', 'b', 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 'libero nam.tiff');
+insert into categories (id, category, question, example, image) values (3, 'a', 'c', 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.', 'mi sit amet.tiff');
+insert into categories (id, category, question, example, image) values (4, 'a', 'd', 'Phasellus in felis. Donec semper sapien a libero. Nam dui.', 'quis libero nullam.jpeg');
+insert into categories (id, category, question, example, image) values (5, 'a', 'e', 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.', 'consequat.gif');
+insert into categories (id, category, question, example, image) values (6, 'a', 'f', 'Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.', 'arcu.jpeg');
+insert into categories (id, category, question, example, image) values (7, 'a', 'g', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 'imperdiet.jpeg');
+insert into categories (id, category, question, example, image) values (8, 'a', 'h', 'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.', 'posuere cubilia curae.tiff');
+insert into categories (id, category, question, example, image) values (9, 'a', 'i', 'Phasellus in felis. Donec semper sapien a libero. Nam dui.', 'in faucibus orci.gif');
