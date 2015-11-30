eacd7ddf99d7698b2daf4d052b644efa113c3802
Tomoko Kawano <Tomoko@MacBook-Pro.local>
2015-11-30 10:33:41 +0900

    refine

---
+++ Design/assets/js/skrollspy.js
@@ -0,0 +1,27 @@
+$function(){
+
+    $('nav.affix-nav').affix({
+        offset:{
+            top: 300,
+            bottom:300
+        }
+    }).on('affix.bs.affix', function () {
+        $(this).css({
+                'top': '0'
+        });
+        console.log('affix.bs.affix');
+    }).on('affixed.bs.affix', function(){
+        console.log('affixed.bs.affix');
+    }).on('affix-bottom.bs.affix', function(){
+        console.log('affix-bottom.bs.affix');
+    }).on('affixed-bottom.bs.affix', function(){
+        console.log('affixed-bottom.bs.affix');
+    });
+
+    $('body').scrollspy({
+     target: 'nav.affix-nav'
+      })
+
+};
+
+// })(jQuery);
